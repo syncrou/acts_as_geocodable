bef3cb6bfb9ba025097f22d27488d329296fef54
Drew Bomhof <syncrou@users.noreply.github.com>
2016-11-14 07:14:45 -0500

    Update geocoding.rb
    Another commit from github.com/osaris resolving the mass assignment errors

--- lib/acts_as_geocodable/geocoding.rb
+++ lib/acts_as_geocodable/geocoding.rb
@@ -1,6 +1,8 @@
 class Geocoding < ActiveRecord::Base
   belongs_to :geocode
   belongs_to :geocodable, polymorphic: true
+  
+  attr_accessible :geocode
 
   def self.geocoded_class(geocodable)
     ActiveRecord::Base.send(:class_name_of_active_record_descendant, geocodable.class).to_s
